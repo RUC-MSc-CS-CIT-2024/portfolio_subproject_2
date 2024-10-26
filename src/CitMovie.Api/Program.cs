87797f3afe78cb935cb14a008b6a0fe8c0d90a0e
sofushn <sofushn@hotmail.dk>
2024-10-26 14:34:48 +0200

    feat: setup https

--- src/CitMovie.Api/Program.cs
+++ src/CitMovie.Api/Program.cs
@@ -10,12 +10,22 @@ builder.Services.AddDbContext<FrameworkContext>(options =>
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));;
 
+builder.Services.AddControllers();
+
+builder.Services.AddHttpsRedirection(options =>
+{
+    options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+    options.HttpsPort = 5001;
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+} else {
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
