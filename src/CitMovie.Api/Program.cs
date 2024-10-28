9ffaffc5173df54e4a377c1b54370fc18b3e6835
sofushn <sofushn@hotmail.dk>
2024-10-28 01:01:36 +0100

    feat: add authentication and authorization to api

--- src/CitMovie.Api/Program.cs
+++ src/CitMovie.Api/Program.cs
@@ -1,7 +1,10 @@
-using CitMovie.Business;
+using System.Security.Claims;
+using System.Text;
 using CitMovie.Data;
 using CitMovie.Data.FollowRepository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,6 +30,47 @@ builder.Services.AddHttpsRedirection(options =>
     options.HttpsPort = 5001;
 });
 
+
+
+builder.Services
+    .AddAuthentication(options => {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options => {
+        JwtOptions jwtSettings = builder.Configuration.GetSection("JwtOptions").Get<JwtOptions>()
+            ?? new JwtOptions() {
+                Issuer = builder.Configuration.GetValue<string>("JWT_ISSUER") ?? throw new Exception("JWT Issuer not found"),
+                Audience = builder.Configuration.GetValue<string>("JWT_AUDIENCE") ?? throw new Exception("JWT Audience not found"),
+                SigningKey = builder.Configuration.GetValue<string>("JWT_SIGNING_KEY") ?? throw new Exception("JWT Signing Key not found")
+            };
+        jwtSettings.Issuer = builder.Configuration.GetValue<string>("JWT_ISSUER") ?? jwtSettings.Issuer;
+        jwtSettings.Audience = builder.Configuration.GetValue<string>("JWT_AUDIENCE") ?? jwtSettings.Audience;
+        jwtSettings.SigningKey = builder.Configuration.GetValue<string>("JWT_SIGNING_KEY") ?? jwtSettings.SigningKey;
+
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings.Issuer,
+            ValidAudience = jwtSettings.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SigningKey))
+        };
+    });
+
+builder.Services.AddAuthorization(options => {
+    options.AddPolicy("user_scope", policy => {
+        policy.RequireClaim("user_id");
+        policy.RequireAssertion(context => {
+            string? userId = (context.Resource as HttpContext)?.Request.RouteValues["userId"]?.ToString();
+            return userId != null && context.User.FindFirstValue("user_id") == userId;
+        });
+    });
+});
+
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -37,12 +81,15 @@ if (app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.MapControllers();
+
 app.UseHttpsRedirection();
 app.UseRouting();
 
 app.MapControllers();
 
-app.MapControllers();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapGet("/test-connection", async (FrameworkContext context) =>
 {
