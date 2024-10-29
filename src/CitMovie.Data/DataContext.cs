24125d02ff2406972ff6ac5822d5a7adb4c9f175
sofushn <sofushn@hotmail.dk>
2024-10-30 00:17:29 +0100

    fix: media models

--- src/CitMovie.Data/DataContext.cs
+++ src/CitMovie.Data/DataContext.cs
@@ -1,6 +1,4 @@
-using CitMovie.Models.DomainObjects;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 
@@ -11,14 +9,15 @@ public class DataContext : DbContext
     private readonly string? _connectionString;
 
     public DbSet<CastMember> CastMembers { get; set; }
-    public DbSet<Collection> Collections { get; set; }
+    public DbSet<MediaCollection> Collections { get; set; }
     public DbSet<Country> Countries { get; set; }
     public DbSet<CrewMember> CrewMembers { get; set; }
-    public DbSet<Episode> Episodes { get; set; }
     public DbSet<Genre> Genres { get; set; }
     public DbSet<JobCategory> JobCategories { get; set; }
     public DbSet<Language> Languages { get; set; }
     public DbSet<Media> Media { get; set; }
+    public DbSet<Season> Seasons { get; set; }
+    public DbSet<Episode> Episodes { get; set; }
     public DbSet<MediaProductionCompany> MediaProductionCompanies { get; set; }
     public DbSet<Person> People { get; set; }
     public DbSet<ProductionCompany> ProductionCompanies { get; set; }
@@ -26,7 +25,6 @@ public class DataContext : DbContext
     public DbSet<RelatedMedia> RelatedMedia { get; set; }
     public DbSet<Release> Releases { get; set; }
     public DbSet<Score> Scores { get; set; }
-    public DbSet<Season> Seasons { get; set; }
     public DbSet<Title> Titles { get; set; }
     public DbSet<TitleAttribute> TitleAttributes { get; set; }
     public DbSet<TitleType> TitleTypes { get; set; }
@@ -49,6 +47,7 @@ public class DataContext : DbContext
     {
         modelBuilder.Entity<MediaProductionCompany>()
             .HasKey(od => new { od.MediaId, od.ProductionCompanyId });
-
+        
+        modelBuilder.Entity<Media>().UseTptMappingStrategy();    
     }
 }
--- src/CitMovie.Models/DomainObjects/Season.cs
+++ src/CitMovie.Models/DomainObjects/Season.cs
@@ -1,21 +1,19 @@
 namespace CitMovie.Models.DomainObjects;
 
-[Table("Season")]
-public class Season
+[Table("season")]
+public class Season : Media
 {
-    [Key, Column("media_id")]
-    public int MediaId { get; set; }
     [Column("status")]
-    public string Status { get; set; }
-    [Column("season_number")]
-    public int SeasonNumber { get; set; }
-    [Column("end_date")]
-    public DateTime EndDate { get; set; }
-    [Column("series_id")]
-    public int SeriesId { get; set; }
+    public required string Status { get; set; }
 
-    [ForeignKey(nameof(MediaId))]
-    public Media Media { get; set; }
-    [ForeignKey(nameof(SeriesId))]
-    public Media Series { get; set; }
+    [Column("season_number")]
+    public int? SeasonNumber { get; set; }
+
+    [Column("end_date")]
+    public DateTime? EndDate { get; set; }
+
+    [Column("series_id")]
+    public required int SeriesId { get; set; }
+
+    public required Media Series { get; set; }
 }
