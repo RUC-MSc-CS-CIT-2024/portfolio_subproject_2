d5ff75e2bdb5d16b3101369061bed90c0e07ccb5
sofushn <sofushn@hotmail.dk>
2024-10-31 23:16:23 +0100

    feat: implement media repository search

--- src/CitMovie.Data/Repositories/IMediaRepository.cs
+++ src/CitMovie.Data/Repositories/IMediaRepository.cs
@@ -1,5 +1,12 @@
 namespace CitMovie.Data;
 
 public interface IMediaRepository {
-    IEnumerable<Media> GetAllMedia();
+    IEnumerable<Media> SearchBestMatch(string[] keywords, int page, int pageSize);
+    IEnumerable<Media> SearchExactMatch(string[] keywords, int page, int pageSize);
+    IEnumerable<Media> SearchSimple(string query, int userId, int page, int pageSize);
+    IEnumerable<Media> SearchStructured(string title, string plot, string character, string person, int userId, int page, int pageSize);
+    IEnumerable<Media> GetAllMedia(int page, int pageSize);
+    IEnumerable<Media> GetAllMedia(Func<Media, bool> wherePredicate, int page, int pageSize);
+    IEnumerable<Media> GetSimilarMedia(int id, int page, int pageSize);
+    Media? GetMediaById(int id);
 }
--- src/CitMovie.Data/Repositories/MediaRepository.cs
+++ src/CitMovie.Data/Repositories/MediaRepository.cs
@@ -8,10 +8,115 @@ public class MediaRepository : IMediaRepository {
         _context = context;
     }
 
-    IEnumerable<Media> IMediaRepository.GetAllMedia()
+    public IEnumerable<Media> GetAllMedia(int page, int pageSize)
+        => _context.Media
+            .Include(x => x.PrimaryInformation)
+                .ThenInclude(x => x.Title)
+            .Include(x => x.PrimaryInformation)
+                .ThenInclude(x => x.Release)
+            .Include(x => x.PrimaryInformation)
+                .ThenInclude(x => x.PromotionalMedia)
+            .Skip(pageSize * page - 1)
+            .Take(pageSize);
+
+    public IEnumerable<Media> GetAllMedia(Func<Media, bool> wherePredicate, int page, int pageSize)
+        => _context.Media
+            .Include(x => x.PrimaryInformation)
+                .ThenInclude(x => x.Title)
+            .Include(x => x.PrimaryInformation)
+                .ThenInclude(x => x.Release)
+            .Include(x => x.PrimaryInformation)
+                .ThenInclude(x => x.PromotionalMedia)
+            .Where(wherePredicate)
+            .Skip(pageSize * page - 1)
+            .Take(pageSize);
+
+    public Media? GetMediaById(int id)
+        => _context.Media
+            .Include(m => m.Genres)
+            .Include(m => m.Scores)
+            .Include(m => m.MediaProductionCompany)
+            .Include(m => m.Countries)
+            .Include(x => x.PrimaryInformation)
+                .ThenInclude(x => x.Title)
+            .Include(x => x.PrimaryInformation)
+                .ThenInclude(x => x.Release)
+            .Include(x => x.PrimaryInformation)
+                .ThenInclude(x => x.PromotionalMedia)
+            .FirstOrDefault(m => m.Id == id);
+
+    public IEnumerable<Media> GetSimilarMedia(int id, int page, int pageSize)
     {
-        return _context.Media
-            .Where(x => x.Type == "tvSeason")
-            .Take(10);
+        IEnumerable<int> searchResult = _context
+            .GetSimilarMedia(id)
+            .Skip(pageSize * (page - 1))
+            .Take(pageSize)
+            .Select(x => x.Id)
+            .Distinct()
+            .ToList();
+
+        return GetMediaFromIds(searchResult);
     }
+
+    public IEnumerable<Media> SearchBestMatch(string[] keywords, int page, int pageSize) 
+    {
+        IEnumerable<int> searchResult = _context
+            .BestMatchSearch(keywords)
+            .Skip(pageSize * (page - 1))
+            .Take(pageSize)
+            .Select(x => x.Id)
+            .Distinct()
+            .ToList();
+        
+        return GetMediaFromIds(searchResult);
+    }
+
+    public IEnumerable<Media> SearchExactMatch(string[] keywords, int page, int pageSize) 
+    {
+        IEnumerable<int> searchResult = _context
+            .ExactMatchSearch(keywords)
+            .Skip(pageSize * (page - 1))
+            .Take(pageSize)
+            .Select(x => x.Id)
+            .Distinct()
+            .ToList();
+
+        return GetMediaFromIds(searchResult);
+    }
+        
+    public IEnumerable<Media> SearchSimple(string query, int userId, int page, int pageSize) 
+    {
+        IEnumerable<int> searchResult = _context
+            .SimpleSearch(query, userId)
+            .Skip(pageSize * (page - 1))
+            .Take(pageSize)
+            .Select(x => x.Id)
+            .Distinct()
+            .ToList();
+
+        return GetMediaFromIds(searchResult);
+    }
+
+    public IEnumerable<Media> SearchStructured(string? title, string? plot, string? character, string? person, int userId, int page, int pageSize)
+    {
+        IEnumerable<int> searchResult = _context
+            .StructuredSearch(title, plot, character, person, userId)
+            .Skip(pageSize * (page - 1))
+            .Take(pageSize)
+            .Select(x => x.Id)
+            .Distinct()
+            .ToList();
+
+        return GetMediaFromIds(searchResult);
+    }
+
+    private IQueryable<Media> GetMediaFromIds(IEnumerable<int> ids)
+        => _context.Media
+            .Where(x => ids.Contains(x.Id))
+            .Include(x => x.PrimaryInformation)
+                .ThenInclude(x => x.Title)
+            .Include(x => x.PrimaryInformation)
+                .ThenInclude(x => x.Release)
+            .Include(x => x.PrimaryInformation)
+                .ThenInclude(x => x.PromotionalMedia);
 }
