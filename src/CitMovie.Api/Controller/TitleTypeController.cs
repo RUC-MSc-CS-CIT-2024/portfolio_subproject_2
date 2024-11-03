ebdaa76b36a1c0bafadd5d1f0efe7b9db7c69936
idax6797 <idahay24@gmail.com>
2024-11-03 10:58:52 +0100

    feat: set up pagingHelper class that can be injected into the controlleres when needed. Refactor the paging logic to take a additional route value

--- src/CitMovie.Api/Controller/TitleTypeController.cs
+++ src/CitMovie.Api/Controller/TitleTypeController.cs
@@ -5,12 +5,12 @@ namespace CitMovie.Api;
 public class TitleTypeController : ControllerBase
 {
     private readonly ITitleTypeManager _titleTypeManager;
-    private readonly LinkGenerator _linkGenerator;
+    private readonly PagingHelper _pagingHelper;
 
-    public TitleTypeController(ITitleTypeManager titleTypeManager, LinkGenerator linkGenerator)
+    public TitleTypeController(ITitleTypeManager titleTypeManager, PagingHelper pagingHelper)
     {
         _titleTypeManager = titleTypeManager;
-        _linkGenerator = linkGenerator;
+        _pagingHelper = pagingHelper;
     }
 
     [HttpGet(Name = nameof(GetTitleTypes))]
@@ -18,46 +18,9 @@ public class TitleTypeController : ControllerBase
     {
         var titleTypes = await _titleTypeManager.GetTytleTypesAsync(page, pageSize);
         var total_items = await _titleTypeManager.GetTotalTitleTypeCountAsync();
-        
-        var result = CreatePaging(nameof(GetTitleTypes), page, pageSize, total_items, titleTypes);
-        
+
+        var result = _pagingHelper.CreatePaging(nameof(GetTitleTypes), page, pageSize, total_items, titleTypes);
+
         return Ok(result);
-    }
-    
-    
-    private string? GetLink(string linkName, int page, int pageSize)
-    {
-        var uri = _linkGenerator.GetUriByName(
-            HttpContext,
-            linkName,
-            new {page, pageSize }
-        );
-        return uri;
-    }
-
-    private object CreatePaging<T>(string linkName, int page, int pageSize, int total, IEnumerable<T?> items)
-    {
-        var numberOfPages = (int)Math.Ceiling(total / (double)pageSize);
-
-        var curPage = GetLink(linkName, page, pageSize);
-
-        var nextPage = page < numberOfPages - 1
-            ? GetLink(linkName, page + 1, pageSize)
-            : null;
-
-        var prevPage = page > 0
-            ? GetLink(linkName, page - 1, pageSize)
-            : null;
-
-        var result = new
-        {
-            CurPage = curPage,
-            NextPage = nextPage,
-            PrevPage = prevPage,
-            NumberOfItems = total,
-            NumberPages = numberOfPages,
-            Items = items
-        };
-        return result;
     }
 }
