03601cc2359a17d931e9adf16d8f8935891c7f44
idax6797 <idahay24@gmail.com>
2024-11-05 08:16:12 +0100

    feat: set up self resource lining on user controller

--- src/CitMovie.Models/DataTransferObjects/UserDto.cs
+++ src/CitMovie.Models/DataTransferObjects/UserDto.cs
@@ -1,5 +1,11 @@
 namespace CitMovie.Models.DataTransferObjects;
 
-public record UserResult(int Id, string Username, string Email);
+public class UserResult
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public List<Link> Links { get; set; } = new List<Link>();
+}
 public record UserCreateRequest(string Username, string Email, string Password);
 public record UserUpdateRequest(string? Username, string? Email, string? Password);
