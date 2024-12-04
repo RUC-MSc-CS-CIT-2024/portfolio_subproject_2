d497652b2c2f04640cb13035edc08ae1ab79f471
sofushn <sofushn@hotmail.dk>
2024-12-05 00:59:48 +0100

    feat: add function for generating refresh token

--- src/CitMovie.Business/IJwtTokenGenerator.cs
+++ src/CitMovie.Business/IJwtTokenGenerator.cs
@@ -3,4 +3,6 @@ namespace CitMovie.Business;
 public interface IJwtTokenGenerator
 {
     string GenerateEncodedToken(User user, IEnumerable<string> roles);
+    string GenerateRefreshToken(string tokenId, int userId);
+    (string tokenId, int userId) ParseRefreshTokenPayload(string token);
 }
