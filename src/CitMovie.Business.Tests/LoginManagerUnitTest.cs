6e8fef4dc32e057be246ff2a884a22055de0a030
sofushn <sofushn@hotmail.dk>
2024-12-05 01:01:35 +0100

    feat: extend login manager with functions for refreshtoken auth and revokation

--- src/CitMovie.Business.Tests/LoginManagerUnitTest.cs
+++ src/CitMovie.Business.Tests/LoginManagerUnitTest.cs
@@ -1,14 +1,16 @@
+using System.Security.Cryptography;
 using CitMovie.Data;
 using CitMovie.Models.DomainObjects;
 using FakeItEasy;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CitMovie.Business.Tests;
 
 public class LoginManagerUnitTest
 {
     [Fact]
-    public async Task LoginAsync_PasswordDoesNotMatch_UnauthorizedAccessExceptionIsThrown()
+    public async Task AuthenticateAsync_PasswordDoesNotMatch_UnauthorizedAccessExceptionIsThrown()
     {
         // Arrange
         IUserRepository repository = A.Fake<IUserRepository>();
@@ -17,18 +19,21 @@ public class LoginManagerUnitTest
         IPasswordHelper passwordHelper = A.Fake<IPasswordHelper>();
         A.CallTo(() => passwordHelper.VerifyPassword(A.Dummy<string>(), A.Dummy<string>(), A.Dummy<string>())).Returns(false);
 
-        LoginManager userRepository = new LoginManager(repository, A.Fake<IJwtTokenGenerator>(), passwordHelper);
+        RefreshTokenCache refreshTokenCache = A.Fake<RefreshTokenCache>();
+        A.CallTo(() => refreshTokenCache.Generate(A<int>.Ignored)).Returns("refreshToken");
+
+        LoginManager userRepository = new LoginManager(repository, A.Fake<IJwtTokenGenerator>(), passwordHelper, refreshTokenCache, CreateFakeJwtOptions());
         
 
         // Act
-        Func<Task> testCode = () => userRepository.LoginAsync("username", "invalidPassword");
+        Func<Task> testCode = () => userRepository.AuthenticateAsync("username", "invalidPassword");
 
         // Act & Assert
         await Assert.ThrowsAsync<UnauthorizedAccessException>(testCode);
     }
 
     [Fact]
-    public async Task LoginAsync_PasswordMatch_JwtTokenIsReturned()
+    public async Task AuthenticateAsync_PasswordMatch_JwtTokenIsReturned()
     {
         // Arrange
         IUserRepository repository = A.Fake<IUserRepository>();
@@ -40,12 +45,35 @@ public class LoginManagerUnitTest
         IJwtTokenGenerator jwtTokenGenerator = A.Fake<IJwtTokenGenerator>();
         A.CallTo(() => jwtTokenGenerator.GenerateEncodedToken(A<User>.Ignored, A<IEnumerable<string>>.Ignored)).Returns("token");
 
-        LoginManager userRepository = new LoginManager(repository, jwtTokenGenerator, passwordHelper);
+        RefreshTokenCache refreshTokenCache = A.Fake<RefreshTokenCache>();
+        A.CallTo(() => refreshTokenCache.Generate(A<int>.Ignored)).Returns("refreshToken");
+
+        LoginManager userRepository = new LoginManager(repository, A.Fake<IJwtTokenGenerator>(), passwordHelper, refreshTokenCache, CreateFakeJwtOptions());
 
         // Act
-        string token = await userRepository.LoginAsync("username", "password");
+        TokenDto token = await userRepository.AuthenticateAsync("username", "password");
 
         // Assert
-        Assert.Equal("token", token);
+        Assert.Equal("token", token.AccessToken);
+    }
+
+    // AuthenticateAsync_InvalidRefreshToken_
+    // AuthenticateAsync_ExpiredRefreshToken_
+    // AuthenticateAsync_ValidRefreshToken_
+
+    // RevokeRefreshToken_InvalidRefreshToken_
+    // RevokeRefreshToken_ExpiredRefreshToken_
+    // RevokeRefreshToken_ValidRefreshToken_TokenIsRevokedAndCannotBeUsedAgain
+
+    private static IOptionsSnapshot<JwtOptions> CreateFakeJwtOptions() {
+        JwtOptions jwtConf = new() {
+            Audience = "audience",
+            Issuer = "issuer",
+            SigningKey = new RsaSecurityKey(new RSACryptoServiceProvider(512)).ToString()
+        };
+        IOptionsSnapshot<JwtOptions> jwtSettings = A.Fake<IOptionsSnapshot<JwtOptions>>();
+        A.CallTo(() => jwtSettings.Value).Returns(jwtConf);
+
+        return jwtSettings;
     }
 }
--- src/CitMovie.Business/Managers/LoginManager.cs
+++ src/CitMovie.Business/Managers/LoginManager.cs
@@ -5,30 +5,59 @@ public class LoginManager : ILoginManager
     private readonly IUserRepository _repository;
     private readonly IJwtTokenGenerator _jwtTokenGenerator;
     private readonly IPasswordHelper _passwordHelper;
+    private readonly IRefreshTokenCache _refreshTokenCache;
+    private readonly JwtOptions _jwtOptions;
 
-    public LoginManager(IUserRepository repository, IJwtTokenGenerator jwtTokenGenerator, IPasswordHelper passwordHelper)
+    public LoginManager(
+        IUserRepository repository, 
+        IJwtTokenGenerator jwtTokenGenerator, 
+        IPasswordHelper passwordHelper, 
+        IRefreshTokenCache refreshTokenCache,
+        IOptionsSnapshot<JwtOptions> jwtOptionsAccessor)
     {
         _jwtTokenGenerator = jwtTokenGenerator;
         _passwordHelper = passwordHelper;
+        _refreshTokenCache = refreshTokenCache;
+
+        _jwtOptions = jwtOptionsAccessor.Value;
+
         _repository = repository;
     }
 
-    public async Task<string> LoginAsync(string username, string password)
+    public async Task<TokenDto> AuthenticateAsync(string username, string password)
     {
         User user = await _repository.GetUserAsync(username);
         if (!_passwordHelper.VerifyPassword(password, user.Salt, user.HashedPassword))
             throw new UnauthorizedAccessException();
 
-        return _jwtTokenGenerator.GenerateEncodedToken(user, new List<string> {"default"}); 
+        TokenDto token = new TokenDto
+        {
+            AccessToken = _jwtTokenGenerator.GenerateEncodedToken(user, new List<string> {"default"}),
+            RefreshToken = _refreshTokenCache.Generate(user.Id),
+            ExpiresIn = _jwtOptions.TokenLifetimeSeconds
+        };
+        return token;
+    }   
+
+    public async Task<TokenDto> AuthenticateAsync(string refreshToken)
+    {
+        (string tokenId, int userId) = _jwtTokenGenerator.ParseRefreshTokenPayload(refreshToken);
+
+        if (!_refreshTokenCache.IsValid(tokenId))
+            throw new UnauthorizedAccessException();
+
+        User user = await _repository.GetUserAsync(userId);
+        return new TokenDto
+        {
+            AccessToken = _jwtTokenGenerator.GenerateEncodedToken(user, new List<string> {"default"}),
+            RefreshToken = _refreshTokenCache.Generate(userId),
+            ExpiresIn = _jwtOptions.TokenLifetimeSeconds
+        };
     }
 
-    public Task<string> RefreshTokenAsync(string token)
+    public void RevokeRefreshToken(string refreshToken)
     {
-        throw new NotImplementedException();
-    }
-
-    public Task Revoke(string token)
-    {
-        throw new NotImplementedException();
+        (string tokenId, _) = _jwtTokenGenerator.ParseRefreshTokenPayload(refreshToken);
+        _refreshTokenCache.Revoke(tokenId);
     }
 }
