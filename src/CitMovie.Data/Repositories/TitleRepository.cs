606752e136bf111c8496975ce498655906c35f0c
Sofus Hilfling <sofushn@hotmail.dk>
2024-12-16 00:41:56 +0100

    fix: paging for titles (#107)

--- src/CitMovie.Data/Repositories/TitleRepository.cs
+++ src/CitMovie.Data/Repositories/TitleRepository.cs
@@ -38,12 +38,16 @@ public class TitleRepository : ITitleRepository {
             .Include(x => x.TitleAttributes)
             .First(x => x.Id == titleId);
 
-    public IEnumerable<Title> GetForMedia(int mediaId)
+    public IEnumerable<Title> GetForMedia(int mediaId, int page, int count)
         => _context.Titles
             .AsNoTracking()
             .Include(x => x.TitleAttributes)
             .Include(x => x.TitleTypes)
-            .Where(x => x.MediaId == mediaId);
+            .Where(x => x.MediaId == mediaId)
+            .OrderBy(x => x.Name)
+            .ThenBy(x => x.Id)
+            .Pagination(page, count)
+            .ToList();
 
     public int GetTotalTitles(int mediaId)
         => _context.Titles
