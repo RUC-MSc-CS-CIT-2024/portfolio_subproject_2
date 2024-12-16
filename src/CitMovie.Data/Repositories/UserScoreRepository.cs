389673a0e073729b16174205d1d1eb014d36a313
Sofus Hilfling <sofus@techchapter.com>
2024-12-16 23:24:04 +0100

    fix: user score endpoint

--- src/CitMovie.Data/Repositories/UserScoreRepository.cs
+++ src/CitMovie.Data/Repositories/UserScoreRepository.cs
@@ -14,44 +14,94 @@ public class UserScoreRepository : IUserScoreRepository
 
     public async Task<IEnumerable<UserScore>> GetUserScoresAsync(int userId, int page, int pageSize, string? mediaType, int? mediaId, string? mediaName)
     {
+        if (page < 1 || pageSize < 1)
+            return [];
+
         var userScoresQuery = _frameworkContext.UserScores
             .AsNoTracking()
             .Where(us => us.UserId == userId);
 
-        var mediaQuery = _dataContext.Media.AsQueryable();
-        if (!string.IsNullOrEmpty(mediaType))
-        {
-            mediaQuery = mediaQuery.Where(m => m.Type == mediaType);
-        }
-        if (mediaId.HasValue && mediaId > 0)
-        {
-            mediaQuery = mediaQuery.Where(m => m.Id == mediaId);
-        }
-
-        var titlesQuery = _dataContext.Titles.AsQueryable();
-        if (!string.IsNullOrEmpty(mediaName))
-        {
-            titlesQuery = titlesQuery.Where(t => t.Name.Contains(mediaName));
+        if (mediaId.HasValue) {
+            Media? m = _dataContext.Media.Include(x => x.Titles).FirstOrDefault(m => m.Id == mediaId);
+            if (!string.IsNullOrEmpty(mediaType) && m is not null && m.Type != mediaType)
+                return [];
+            else if (!string.IsNullOrEmpty(mediaName) && m is not null && !m.Titles.Any(t => t.Name.Contains(mediaName)))
+                return [];
+            IQueryable<UserScore> userScoreWithIdQuery = userScoresQuery.Where(us => us.MediaId == mediaId);
+            // No need to paginate as there can only be one result with MediaId 
+            UserScore? us = userScoreWithIdQuery.FirstOrDefault(us => us.CreatedAt == userScoreWithIdQuery.Max(us => us.CreatedAt));
+            return us is null ? [] : [us];
         }
 
-        var mediaIds = new HashSet<int>(await mediaQuery.Select(m => m.Id).ToListAsync());
-        var titleMediaIds = new HashSet<int>(await titlesQuery.Select(t => t.MediaId).ToListAsync());
+        List<int> ids = [];
+        if (!string.IsNullOrEmpty(mediaType)) {
+            var mediaTypeMatchIds = await _dataContext.Media
+                .Where(m => m.Type == mediaType)
+                .Select(m => m.Id)
+                .ToListAsync();
+            ids.AddRange(mediaTypeMatchIds);
+        }
+        if (!string.IsNullOrEmpty(mediaName)) {
+            var titleMatchIds = await _dataContext.Titles
+                .Where(t => t.Name.Contains(mediaName))
+                .Select(t => t.MediaId)
+                .ToListAsync();
+            ids.AddRange(titleMatchIds);
+        }
+        if (ids.Count > 0) {
+            ids = ids.Distinct().ToList();
+            userScoresQuery = userScoresQuery.Where(us => ids.Contains(us.MediaId));
+        }
         
         var filteredUserScores = await userScoresQuery
-            .Where(us => mediaIds.Contains(us.MediaId) && (string.IsNullOrEmpty(mediaName) || titleMediaIds.Contains(us.MediaId)))
             .GroupBy(us => us.MediaId)
-            .Select(g => g.OrderByDescending(us => us.CreatedAt).First())
-            .OrderBy(us => us.UserScoreId)
-            .Pagination(page, pageSize)
+            .Select(g => g.First(us => us.CreatedAt == g.Max(us => us.CreatedAt)))
             .ToListAsync();
 
-        return filteredUserScores;
+        return filteredUserScores
+            .AsQueryable()
+            .OrderBy(us => us.UserScoreId)
+            .Pagination(page, pageSize)
+            .ToList();
     }
 
-    public async Task<int> GetTotalUserScoresCountAsync(int userId)
+    public async Task<int> GetTotalUserScoresCountAsync(int userId, string? mediaType, int? mediaId, string? mediaName)
     {
-        return await _frameworkContext.UserScores
-            .CountAsync(us => us.UserId == userId);
+        var userScoresQuery = _frameworkContext.UserScores
+            .Where(us => us.UserId == userId);
+
+        if(mediaId.HasValue) {
+            Media? m = _dataContext.Media.Include(x => x.Titles).FirstOrDefault(m => m.Id == mediaId);
+            if (!string.IsNullOrEmpty(mediaType) && m is not null && m.Type != mediaType)
+                return 0;
+            else if (!string.IsNullOrEmpty(mediaName) && m is not null && !m.Titles.Any(t => t.Name.Contains(mediaName)))
+                return 0;
+            return _frameworkContext.UserScores.Any(us => us.UserId == userId && us.MediaId == mediaId) ? 1 : 0;
+        }
+
+        List<int> ids = [];
+        if (!string.IsNullOrEmpty(mediaType)) {
+            var mediaTypeMatchIds = await _dataContext.Media
+                .Where(m => m.Type == mediaType)
+                .Select(m => m.Id)
+                .ToListAsync();
+            ids.AddRange(mediaTypeMatchIds);
+        }
+        if (!string.IsNullOrEmpty(mediaName)) {
+            var titleMatchIds = await _dataContext.Titles
+                .Where(t => t.Name.Contains(mediaName))
+                .Select(t => t.MediaId)
+                .ToListAsync();
+            ids.AddRange(titleMatchIds);
+        }
+        if (ids.Count > 0) {
+            ids = ids.Distinct().ToList();
+            userScoresQuery = userScoresQuery.Where(us => ids.Contains(us.MediaId));
+        }
+
+        return await userScoresQuery
+            .GroupBy(us => us.MediaId)
+            .CountAsync();
     }
 
     public async Task CreateUserScoreAsync(int userId, int mediaId, int score, string? reviewText)
